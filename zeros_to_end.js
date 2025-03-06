d161963937d7b841085dcd22828ac5c7e2c15496
Gaurav Sharma <sgrb26@gmail.com>
2025-03-06 23:17:20 +0530

    zeros to end

---
+++ zeros_to_end.js
@@ -0,0 +1,20 @@
+const moveZerosToEnd = (arr) => {
+  let index = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] != 0) {
+      [arr[i], arr[index]] = [arr[index], arr[i]]
+      index += 1;
+    }
+  }
+  return arr;
+}
+
+let arr = [8, 5, 0, 10, 0, 20]
+console.log(moveZerosToEnd(arr))
+
+arr = [0, 0, 0, 10, 0]
+console.log(moveZerosToEnd(arr))
+
+arr = [10, 20]
+console.log(moveZerosToEnd(arr))
