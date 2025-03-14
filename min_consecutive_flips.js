562ef64610de7e5e5d78182100a06a7d40036bcf
Gaurav <sgrb26@gmail.com>
2025-03-14 23:19:29 +0530

    boilerplate for min consecutive flips

---
+++ min_consecutive_flips.js
@@ -0,0 +1,12 @@
+const minConsecutiveFlips = (arr) => {
+  
+}
+
+let arr = [1, 1, 0, 0, 0, 1]
+console.log(minConsecutiveFlips(arr))
+
+arr = [1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1]
+console.log(minConsecutiveFlips(arr))
+
+arr = [1, 1, 1];
+console.log(minConsecutiveFlips(arr))
