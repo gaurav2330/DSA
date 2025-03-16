87ff3686b705f0456b80df80a3e8065fc6617794
Gaurav Sharma <sgrb26@gmail.com>
2025-03-16 11:52:48 +0530

    min consecutive flips solution

--- min_consecutive_flips.js
+++ min_consecutive_flips.js
@@ -1,12 +1,30 @@
 const minConsecutiveFlips = (arr) => {
-  
+  let str = '';
+
+  for(let i = 1; i < arr.length; i++) {
+    if (arr[i] != arr[i - 1]) {
+      if (arr[i] != arr[0]) {
+        str = `From ${i} to `
+      }
+      else {
+        str += `${i - 1}`
+        console.log(str)
+      }
+    }
+  }
+  if (str.length > 0) {
+    str += `${arr.length - 1}`
+    console.log(str)
+  }
 }
 
 let arr = [1, 1, 0, 0, 0, 1]
-console.log(minConsecutiveFlips(arr))
+minConsecutiveFlips(arr)
+console.log('###########################')
 
 arr = [1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1]
-console.log(minConsecutiveFlips(arr))
+minConsecutiveFlips(arr)
+console.log('###########################')
 
-arr = [1, 1, 1];
-console.log(minConsecutiveFlips(arr))
+arr = [0, 0, 1, 1, 0, 0, 1, 1, 0, 1];
+minConsecutiveFlips(arr)
