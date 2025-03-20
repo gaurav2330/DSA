d6ecbcf7a4f411b53a32e510263256cded05d277
Gaurav <sgrb26@gmail.com>
2025-03-20 22:55:15 +0530

    search infite array boiler plate

---
+++ Searching/search_infinite_array.js
@@ -0,0 +1,9 @@
+const searchArray = (arr, x) => {
+
+}
+
+let arr = [5, 10, 20, 30, 40, 60, 100, 120, 200, 300,500]
+console.log(searchArray(arr, 100)) // 6
+
+arr = [10, 20, 30, 60, 90, 100]
+console.log(searchArray(arr, 50)) // -1
