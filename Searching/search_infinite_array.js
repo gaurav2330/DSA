32d9a80856ac8c5665b6472d662badea397df750
Gaurav Sharma <sgrb26@gmail.com>
2025-03-21 22:50:25 +0530

    initial logic

--- Searching/search_infinite_array.js
+++ Searching/search_infinite_array.js
@@ -1,9 +1,17 @@
-const searchArray = (arr, x) => {
+const search = (arr, x) => {
+  let start = 0, end = -1;
 
+  if (arr[start] < x) {
+    start *= 2
+  }
+  else if (arr[start] > x) {
+    end = start
+  }
+  return -1;
 }
 
-let arr = [5, 10, 20, 30, 40, 60, 100, 120, 200, 300,500]
-console.log(searchArray(arr, 100)) // 6
+let arr = [1, 10, 15, 20, 40, 80, 90, 100, 120, 150, 200, 210, 230]
+console.log(search(arr, 100)) //7
 
-arr = [10, 20, 30, 60, 90, 100]
-console.log(searchArray(arr, 50)) // -1
+arr = [20, 40, 100, 300, 400, 600, 700, 900]
+console.log(search(arr, 20)) // -1
